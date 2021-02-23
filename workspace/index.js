ca63de7d883dd187c6701260cb54140dc96172a9
Kyung Ho, Park <crorowslowpp@gmail.com>
2021-02-23 17:24:47 +0900

    initial commit
    connected js script to html
    connected css style sheet to html

--- workspace/index.js
+++ workspace/index.js
@@ -7,11 +7,54 @@ function onClickSubmit(event)
     console.log("auaicn");
     console.log(event.target);
 
+    read();
+    // read 가지고 확인
     
+    // 괜찮으면
+    create();
 
+    alert("아이디가 생성되었습니다");
 }
+
 function main()
 {
+    intializeConnection();
+
+    a.addEventListener("submit", onClickSubmit);
+    a.addEventListener("submit", onClickSubmit);
+    a.addEventListener("submit", onClickSubmit);
+    a.addEventListener("submit", onClickSubmit);
+    a.addEventListener("submit", onClickSubmit);
     a.addEventListener("submit", onClickSubmit);
 }
+
+function intializeConnection()
+{
+    // http
+}
+
+function create()
+{
+    // json
+    // send
+}
+
+function read()
+{
+    // json
+    // send
+}
+
+function update()
+{
+    // json
+    // send
+}
+
+function delete()
+{
+    // json
+    // send
+}
+
 main();
