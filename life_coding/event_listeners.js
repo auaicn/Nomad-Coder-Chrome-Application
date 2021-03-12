5e908507dfe736b3e736a95e32d5b8a07e8a6f6f
Kyung Ho, Park <crorowslowpp@gmail.com>
2021-03-13 01:01:51 +0900

    enrolled banilla js sciprt & life coding before SWM coding contest

---
+++ life_coding/event_listeners.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+    var hw = document.getElementById('hw')
+    hw.addEventListener('click',function(){
+        alert('자바스크립트 언어는 세미콜론이 필수적이지 않습니다\n슬래시엔을 통한 line break가 가능합니다')
+    })
+}
