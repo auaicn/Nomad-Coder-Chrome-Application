009b9ffa26f016be9460ccbb0fc1701f449588f0
Kyung Ho, Park <crorowslowpp@gmail.com>
2021-04-21 22:13:23 +0900

    created basic resources

---
+++ index.js
@@ -0,0 +1,35 @@
+console.log('Im Working Im Js Im Beautiful. Im worth it')
+// alert('Im Working Im Js Im Beautiful. Im worth it')
+
+a = 4
+// console.log("a is now " + a)
+
+const daysOfWeek = ["Mon","Mon","Mon","Mon","Mon",true]
+// console.log(daysOfWeek)
+
+const myObject = {
+    a:"7",
+    a:"4",
+    3:"4",
+    key:"4",
+    a:"4",
+    plus: function(a,b){
+        return a+b;
+    }
+}
+
+// console.log(console)
+
+function sayHello(name, age){
+    console.log(`Hello ${name} you are ${age} years old!`)
+}
+
+const ret = sayHello("kyungho Park", 25)
+console.log(myObject.plus(5,5))
+console.log(console)
+
+const title = document.getElementById("title");
+title.innerHTML = "Boo!"
+
+console.dir(title)
+console.log(document)
